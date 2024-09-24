caf904b5f69cdc053146db85675c1a2ae9f9afae
braun1604 <braunda1997@gmail.com>
2024-09-24 10:55:37 +0700

    up card

---
+++ vite-project/data/tasks.js
@@ -0,0 +1,36 @@
+export const Tasks
+{
+    id: 1,
+    topic: "Web Design",
+    title: "Task 1",
+    date: "30.10.2023",
+    status: "Без статуса",
+},
+{
+    id: 2,
+    topic: "Research",
+    title: "Task 2",
+    date: "30.10.2023",
+    status: "Нужно сделать",
+},
+{
+    id: 3,
+    topic: "Copywriting",
+    title: "Task 3",
+    date: "30.10.2023",
+    status: "В работе"
+},
+{
+    id: 4,
+    topic: "Research",
+    title: "Task 4",
+    date: "30.10.2023",
+    status: "Тестирование",
+}
+{
+    id: 5,
+    topic: "Web Design",
+    title: "Task 5",
+    date: "30.10.2023",
+    status: "Готово",
+}
